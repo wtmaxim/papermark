5c60319ec8cf6bb22aee161cbf43b6074213ee3a
maxime <maxime.derame@hotmail.fr>
2025-03-18 15:45:26 +0100

    feat: change middleware and cookie to getpixcel

--- middleware.ts
+++ middleware.ts
@@ -27,6 +27,7 @@ function isCustomDomain(host: string) {
         host?.includes("localhost") ||
         host?.includes("papermark.io") ||
         host?.includes("papermark.com") ||
+        host?.includes("getpixcel.com") ||
         host?.endsWith(".vercel.app")
       ))
   );
